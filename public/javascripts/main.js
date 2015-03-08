93d28f3e6f5c4cdfc9012ed1fae3d0d3e5f26803
sorpa'as plat <me@sorpaas.com>
2015-03-08 14:59:44 +0800

    remove gradient image and canvas

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -2,23 +2,7 @@
 
 var $ = require("jquery");
 
-var gradientImage;
-var gradientCanvas;
-
 $(document).ready(function() {
-    gradientImage = document.createElement('img');
-    gradientImage.src = 'images/star_color_modified.png';
-
-    gradientCanvas = document.createElement('canvas');
-    gradientCanvas.width = gradientImage.width;
-    gradientCanvas.height = gradientImage.height;
-    gradientCanvas.getContext('2d').drawImage(gradientImage, 0, 0,
-                                              gradientImage.width,
-                                              gradientImage.height);
-    gradientCanvas.getColor = function(percentage) {
-        return this.getContext('2d')
-            .getImageData(0, percentage * gradientImage.height, 1, 1).data;
-    }
 
     setTimeout(function() {
         var s = 'scale(1.0)';
